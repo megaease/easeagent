47ae08e38a6db6998f263becdd739d0efe1ab431
zhongl <zhong.lunfu@gmail.com>
2016-12-19 20:04:46 +0800

    Add some java docs.

--- core/src/main/java/com/hexdecteam/easeagent/AbstractTransformation.java
+++ core/src/main/java/com/hexdecteam/easeagent/AbstractTransformation.java
@@ -1,34 +1,51 @@
 package com.hexdecteam.easeagent;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.agent.builder.AgentBuilder.LocationStrategy.ForClassLoader;
+import net.bytebuddy.agent.builder.AgentBuilder.RawMatcher.ForElementMatchers;
+import net.bytebuddy.agent.builder.AgentBuilder.Transformer;
 import net.bytebuddy.description.type.TypeDescription;
-import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.matcher.ElementMatcher.Junction;
 
 import java.lang.instrument.Instrumentation;
 import java.util.Arrays;
 
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
+/**
+ * This is a basic implementation of the {@link Transformation} , that makes your works more easier.
+ * <p>
+ * You properly need to care about two things:
+ * <ul>
+ * <li>What kind of classes would be transform ? And, </li>
+ * <li>how to transform those classes?</li>
+ * </ul>
+ *
+ * @see AgentBuilder
+ */
 public abstract class AbstractTransformation implements Transformation {
     @Override
     public void apply(Instrumentation inst) {
         new AgentBuilder.Default()
-                .with(AgentBuilder.LocationStrategy.ForClassLoader.WEAK)
+                .with(ForClassLoader.WEAK)
                 .with(new DebugListener())
-                .type(excludeSelf().and(matcher()), not(isBootstrapClassLoader()))
-                .transform(transformer())
+                .type(withDescription())
+                .transform(withTransformer())
                 .installOn(inst);
     }
 
-    protected abstract ElementMatcher.Junction<TypeDescription> matcher();
+    protected abstract Junction<TypeDescription> typesMatched();
 
-    protected abstract AgentBuilder.Transformer transformer();
+    protected abstract Transformer withTransformer();
 
-    private ElementMatcher.Junction<TypeDescription> excludeSelf() {
-        return not(nameStartsWith(selfPackagePrefix()));
+    private AgentBuilder.RawMatcher withDescription() {
+        final Junction<TypeDescription> tdm =
+                not(nameStartsWith(packagePrefixOfItSelf())).and(typesMatched());
+
+        return new ForElementMatchers(tdm, not(isBootstrapClassLoader()), any());
     }
 
-    private String selfPackagePrefix() {
+    private String packagePrefixOfItSelf() {
         final String name = Transformation.class.getPackage().getName();
         return Arrays.stream(name.split("\\."))
                      .limit(2)
--- core/src/main/java/com/hexdecteam/easeagent/Transformation.java
+++ core/src/main/java/com/hexdecteam/easeagent/Transformation.java
@@ -2,6 +2,15 @@ package com.hexdecteam.easeagent;
 
 import java.lang.instrument.Instrumentation;
 
+/**
+ * A {@code Transformation} would decorate some java classes during the runtime,
+ * like logging, profiling, tracing and so on.
+ * <p>
+ * Normally, there is no need to implement {@code Transformation} directly, but extend
+ * {@link AbstractTransformation} instead.
+ *
+ * @see AbstractTransformation
+ */
 public interface Transformation {
     void apply(Instrumentation inst);
 }
