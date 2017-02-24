74496249743515b7fd32fa2b1e62d663c669b2e5
zhongl <zhong.lunfu@gmail.com>
2017-02-24 21:08:14 +0800

    Fix Class org.apache.kafka.clients.producer.internals.DefaultPartitioner could not be found.

--- agent/loader/src/main/java/com/hexdecteam/easeagent/Main.java
+++ agent/loader/src/main/java/com/hexdecteam/easeagent/Main.java
@@ -16,26 +16,30 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.jar.Attributes;
 
-/** A main class of java agent to load {@link Agent} service with a {@link LaunchedURLClassLoader}. */
+/**
+ * A main class of java agent to load bootstrap class from a {@link LaunchedURLClassLoader}.
+ */
 public class Main {
     private static final ClassLoader BOOTSTRAP_CLASS_LOADER = null;
-    private static final String      LIB                    = "lib/";
-    private static final String      LOGGING_PROPERTY       = "Logging-Property";
-    private static final String      EASEAGENT_LOG_CONF     = "easeagent.log.conf";
+    private static final String LIB = "lib/";
+    private static final String LOGGING_PROPERTY = "Logging-Property";
+    private static final String EASEAGENT_LOG_CONF = "easeagent.log.conf";
 
     public static void premain(final String args, final Instrumentation inst) throws Exception {
         final JarFileArchive archive = new JarFileArchive(getArchiveFileContains(Main.class));
         final Attributes attributes = archive.getManifest().getMainAttributes();
         final String loggingProperty = attributes.getValue(LOGGING_PROPERTY);
         final String bootstrap = attributes.getValue("Bootstrap-Class");
-        switchLoggingProperty(loggingProperty, EASEAGENT_LOG_CONF, new Callable<Void>() {
+        final URL[] urls = nestArchiveUrls(archive);
+        final ClassLoader loader = new LaunchedURLClassLoader(urls, BOOTSTRAP_CLASS_LOADER);
+
+        switchLoggingProperty(loader, loggingProperty, EASEAGENT_LOG_CONF, new Callable<Void>() {
 
             @Override
             public Void call() throws Exception {
-                final URL[] urls = nestArchiveUrls(archive);
-                final ClassLoader loader = new LaunchedURLClassLoader(urls, BOOTSTRAP_CLASS_LOADER);
-                final Class<?> aClass = loader.loadClass(bootstrap);
-                aClass.getMethod("premain", String.class, Instrumentation.class).invoke(null, args, inst);
+                loader.loadClass(bootstrap)
+                        .getMethod("premain", String.class, Instrumentation.class)
+                        .invoke(null, args, inst);
                 return null;
             }
         });
@@ -45,8 +49,12 @@ public class Main {
      * Switching the system property temporary could fix the problem of conflict of logging configuration
      * when host used the same logging library as agent.
      */
-    private static void switchLoggingProperty(String hostKey, String agentKey, Callable<Void> callable)
+    private static void switchLoggingProperty(ClassLoader loader, String hostKey, String agentKey, Callable<Void> callable)
             throws Exception {
+        final Thread t = Thread.currentThread();
+        final ClassLoader ccl = t.getContextClassLoader();
+        t.setContextClassLoader(loader);
+
         final String host = System.getProperty(hostKey);
         final String agent = System.getProperty(agentKey, "log4j2.xml");
         // Redirect config of host to agent
@@ -54,6 +62,7 @@ public class Main {
         try {
             callable.call();
         } finally {
+            t.setContextClassLoader(ccl);
             // Recovery host configuration
             if (host == null) {
                 System.getProperties().remove(hostKey);
