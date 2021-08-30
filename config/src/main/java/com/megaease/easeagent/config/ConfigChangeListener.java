09dab384fce2986b84dcdcf300e2a0fb5ba3c7a7
jxd134 <jinxiaodong_1990@126.com>
2021-08-30 16:16:55 +0800

    [typo] fix `public` identifier for interface function

--- config/src/main/java/com/megaease/easeagent/config/ConfigChangeListener.java
+++ config/src/main/java/com/megaease/easeagent/config/ConfigChangeListener.java
@@ -20,5 +20,5 @@ package com.megaease.easeagent.config;
 import java.util.List;
 
 public interface ConfigChangeListener {
-    public void onChange(List<ChangeItem> list);
+    void onChange(List<ChangeItem> list);
 }
