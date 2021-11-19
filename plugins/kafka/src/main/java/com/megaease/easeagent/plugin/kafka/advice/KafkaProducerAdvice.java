49de66f869cccb6f46d75c64af8548ad3acfbbda
yufu.deng <observeralone@gmail.com>
2021-11-19 18:43:17 +0800

    add kafka

---
+++ plugins/kafka/src/main/java/com/megaease/easeagent/plugin/kafka/advice/KafkaProducerAdvice.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2017, MegaEase
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.megaease.easeagent.plugin.kafka.advice;
+
+import com.megaease.easeagent.plugin.Points;
+import com.megaease.easeagent.plugin.matcher.ClassMatcher;
+import com.megaease.easeagent.plugin.matcher.IClassMatcher;
+import com.megaease.easeagent.plugin.matcher.IMethodMatcher;
+import com.megaease.easeagent.plugin.matcher.MethodMatcher;
+
+import java.util.Set;
+
+public class KafkaProducerAdvice implements Points {
+    //return def.type(
+    //                named("org.apache.kafka.clients.producer.KafkaProducer")
+    //                .or(hasSuperType(named("org.apache.kafka.clients.producer.MockProducer")))
+    //        )
+    //                .transform(objConstruct(isConstructor().and(takesArguments(7)), AgentDynamicFieldAccessor.DYNAMIC_FIELD_NAME))
+    //                .transform(doSend((named("doSend")
+    //                                .and(isPrivate())
+    //                                .and(takesArguments(2)))
+    //                                .and(takesArgument(0, named("org.apache.kafka.clients.producer.ProducerRecord")))
+    //                                .and(takesArgument(1, named("org.apache.kafka.clients.producer.Callback")))
+    //                                .and(returns(named("java.util.concurrent.Future")))
+    //                        )
+    //
+    //                )
+    //
+    //                .end()
+    //                ;
+    @Override
+    public IClassMatcher getClassMatcher() {
+        return ClassMatcher.builder().hasClassName("org.apache.kafka.clients.producer.KafkaProducer")
+            .build();
+
+    }
+
+    @Override
+    public Set<IMethodMatcher> getMethodMatcher() {
+        return MethodMatcher.multiBuilder()
+            .match(MethodMatcher.builder().named("<init>")
+                .argsLength(7)
+                .qualifier("constructor")
+                .build())
+            .match(MethodMatcher.builder().named("doSend")
+                .isPrivate()
+                .argsLength(2)
+                .arg(0, "org.apache.kafka.clients.producer.ProducerRecord")
+                .arg(1, "org.apache.kafka.clients.producer.Callback")
+                .returnType("java.util.concurrent.Future")
+                .qualifier("doSend")
+                .build())
+            .build();
+    }
+
+//    @Override
+//    public boolean isAddDynamicField() {
+//        return true;
+//    }
+}
