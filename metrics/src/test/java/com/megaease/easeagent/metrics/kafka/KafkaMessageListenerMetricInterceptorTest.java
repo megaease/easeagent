250393a8ba1c92d06774c52790168c4ddb9e9c11
yufu.deng <observeralone@gmail.com>
2021-12-07 10:40:06 +0800

    remove metrics,sniffer and zipkin old test

--- metrics/src/test/java/com/megaease/easeagent/metrics/kafka/KafkaMessageListenerMetricInterceptorTest.java
+++ metrics/src/test/java/com/megaease/easeagent/metrics/kafka/KafkaMessageListenerMetricInterceptorTest.java
@@ -1,117 +1,117 @@
-/*
- * Copyright (c) 2017, MegaEase
- * All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.megaease.easeagent.metrics.kafka;
-
-import com.codahale.metrics.MetricRegistry;
-import com.google.common.collect.Maps;
-import com.megaease.easeagent.config.Config;
-import com.megaease.easeagent.core.interceptor.AgentInterceptorChain;
-import com.megaease.easeagent.plugin.MethodInfo;
-import com.megaease.easeagent.core.utils.ContextUtils;
-import com.megaease.easeagent.metrics.BaseMetricsTest;
-import com.megaease.easeagent.plugin.api.metric.name.NameFactory;
-import com.megaease.easeagent.plugin.api.metric.name.MetricSubType;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.Map;
-
-import static org.mockito.Mockito.mock;
-
-public class KafkaMessageListenerMetricInterceptorTest extends BaseMetricsTest {
-    MetricRegistry metricRegistry;
-    KafkaMetric kafkaMetric;
-    KafkaMessageListenerMetricInterceptor interceptor;
-    String topic = "topic";
-    MethodInfo methodInfo;
-
-    @Before
-    public void before() {
-        Config config = this.createConfig(KafkaMessageListenerMetricInterceptor.ENABLE_KEY, "true");
-        metricRegistry = new MetricRegistry();
-        kafkaMetric = new KafkaMetric(metricRegistry);
-        interceptor = new KafkaMessageListenerMetricInterceptor(kafkaMetric, config);
-
-        ConsumerRecord<String, String> consumerRecord = new ConsumerRecord<>(topic, 1, 0, "key", "value");
-
-        methodInfo = MethodInfo.builder()
-                .args(new Object[]{consumerRecord})
-                .build();
-    }
-
-    @Test
-    public void invokeSuccess() {
-        NameFactory metricNameFactory = NameFactory.createBuilder()
-                .timerType(MetricSubType.CONSUMER, Maps.newHashMap())
-                .meterType(MetricSubType.CONSUMER, Maps.newHashMap())
-                .counterType(MetricSubType.CONSUMER, Maps.newHashMap())
-                .build();
-        Map<Object, Object> context = ContextUtils.createContext();
-
-        AgentInterceptorChain chain = mock(AgentInterceptorChain.class);
-
-        interceptor.before(methodInfo, context, chain);
-        interceptor.after(methodInfo, context, chain);
-
-        Assert.assertEquals(1L, metricRegistry.timer(metricNameFactory.timerName(topic, MetricSubType.CONSUMER)).getCount());
-        Assert.assertEquals(1L, metricRegistry.counter(metricNameFactory.counterName(topic, MetricSubType.CONSUMER)).getCount());
-        Assert.assertEquals(1L, metricRegistry.meter(metricNameFactory.meterName(topic, MetricSubType.CONSUMER)).getCount());
-    }
-
-    @Test
-    public void invokeFail() {
-        Map<Object, Object> context = ContextUtils.createContext();
-        methodInfo.setThrowable(new Exception());
-        AgentInterceptorChain chain = mock(AgentInterceptorChain.class);
-
-        interceptor.before(methodInfo, context, chain);
-        interceptor.after(methodInfo, context, chain);
-
-        NameFactory metricNameFactory = NameFactory.createBuilder()
-                .timerType(MetricSubType.CONSUMER, Maps.newHashMap())
-                .meterType(MetricSubType.CONSUMER, Maps.newHashMap())
-                .meterType(MetricSubType.CONSUMER_ERROR, Maps.newHashMap())
-                .counterType(MetricSubType.CONSUMER, Maps.newHashMap())
-                .counterType(MetricSubType.CONSUMER_ERROR, Maps.newHashMap())
-                .build();
-
-        Assert.assertEquals(1L, metricRegistry.timer(metricNameFactory.timerName(topic, MetricSubType.CONSUMER)).getCount());
-        Assert.assertEquals(1L, metricRegistry.counter(metricNameFactory.counterName(topic, MetricSubType.CONSUMER)).getCount());
-        Assert.assertEquals(1L, metricRegistry.counter(metricNameFactory.counterName(topic, MetricSubType.CONSUMER_ERROR)).getCount());
-        Assert.assertEquals(1L, metricRegistry.meter(metricNameFactory.meterName(topic, MetricSubType.CONSUMER)).getCount());
-        Assert.assertEquals(1L, metricRegistry.meter(metricNameFactory.meterName(topic, MetricSubType.CONSUMER_ERROR)).getCount());
-
-    }
-
-    @Test
-    public void disableCollect() {
-        Config config = this.createConfig(KafkaMessageListenerMetricInterceptor.ENABLE_KEY, "false");
-        metricRegistry = new MetricRegistry();
-        kafkaMetric = new KafkaMetric(metricRegistry);
-        interceptor = new KafkaMessageListenerMetricInterceptor(kafkaMetric, config);
-        Map<Object, Object> context = ContextUtils.createContext();
-        AgentInterceptorChain chain = mock(AgentInterceptorChain.class);
-
-        interceptor.before(methodInfo, context, chain);
-        interceptor.after(methodInfo, context, chain);
-
-        Assert.assertTrue(metricRegistry.getMetrics().isEmpty());
-    }
-}
+///*
+// * Copyright (c) 2017, MegaEase
+// * All rights reserved.
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//package com.megaease.easeagent.metrics.kafka;
+//
+//import com.codahale.metrics.MetricRegistry;
+//import com.google.common.collect.Maps;
+//import com.megaease.easeagent.config.Config;
+//import com.megaease.easeagent.core.interceptor.AgentInterceptorChain;
+//import com.megaease.easeagent.plugin.MethodInfo;
+//import com.megaease.easeagent.core.utils.ContextUtils;
+//import com.megaease.easeagent.metrics.BaseMetricsTest;
+//import com.megaease.easeagent.plugin.api.metric.name.NameFactory;
+//import com.megaease.easeagent.plugin.api.metric.name.MetricSubType;
+//import org.apache.kafka.clients.consumer.ConsumerRecord;
+//import org.junit.Assert;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import java.util.Map;
+//
+//import static org.mockito.Mockito.mock;
+//
+//public class KafkaMessageListenerMetricInterceptorTest extends BaseMetricsTest {
+//    MetricRegistry metricRegistry;
+//    KafkaMetric kafkaMetric;
+//    KafkaMessageListenerMetricInterceptor interceptor;
+//    String topic = "topic";
+//    MethodInfo methodInfo;
+//
+//    @Before
+//    public void before() {
+//        Config config = this.createConfig(KafkaMessageListenerMetricInterceptor.ENABLE_KEY, "true");
+//        metricRegistry = new MetricRegistry();
+//        kafkaMetric = new KafkaMetric(metricRegistry);
+//        interceptor = new KafkaMessageListenerMetricInterceptor(kafkaMetric, config);
+//
+//        ConsumerRecord<String, String> consumerRecord = new ConsumerRecord<>(topic, 1, 0, "key", "value");
+//
+//        methodInfo = MethodInfo.builder()
+//                .args(new Object[]{consumerRecord})
+//                .build();
+//    }
+//
+//    @Test
+//    public void invokeSuccess() {
+//        NameFactory metricNameFactory = NameFactory.createBuilder()
+//                .timerType(MetricSubType.CONSUMER, Maps.newHashMap())
+//                .meterType(MetricSubType.CONSUMER, Maps.newHashMap())
+//                .counterType(MetricSubType.CONSUMER, Maps.newHashMap())
+//                .build();
+//        Map<Object, Object> context = ContextUtils.createContext();
+//
+//        AgentInterceptorChain chain = mock(AgentInterceptorChain.class);
+//
+//        interceptor.before(methodInfo, context, chain);
+//        interceptor.after(methodInfo, context, chain);
+//
+//        Assert.assertEquals(1L, metricRegistry.timer(metricNameFactory.timerName(topic, MetricSubType.CONSUMER)).getCount());
+//        Assert.assertEquals(1L, metricRegistry.counter(metricNameFactory.counterName(topic, MetricSubType.CONSUMER)).getCount());
+//        Assert.assertEquals(1L, metricRegistry.meter(metricNameFactory.meterName(topic, MetricSubType.CONSUMER)).getCount());
+//    }
+//
+//    @Test
+//    public void invokeFail() {
+//        Map<Object, Object> context = ContextUtils.createContext();
+//        methodInfo.setThrowable(new Exception());
+//        AgentInterceptorChain chain = mock(AgentInterceptorChain.class);
+//
+//        interceptor.before(methodInfo, context, chain);
+//        interceptor.after(methodInfo, context, chain);
+//
+//        NameFactory metricNameFactory = NameFactory.createBuilder()
+//                .timerType(MetricSubType.CONSUMER, Maps.newHashMap())
+//                .meterType(MetricSubType.CONSUMER, Maps.newHashMap())
+//                .meterType(MetricSubType.CONSUMER_ERROR, Maps.newHashMap())
+//                .counterType(MetricSubType.CONSUMER, Maps.newHashMap())
+//                .counterType(MetricSubType.CONSUMER_ERROR, Maps.newHashMap())
+//                .build();
+//
+//        Assert.assertEquals(1L, metricRegistry.timer(metricNameFactory.timerName(topic, MetricSubType.CONSUMER)).getCount());
+//        Assert.assertEquals(1L, metricRegistry.counter(metricNameFactory.counterName(topic, MetricSubType.CONSUMER)).getCount());
+//        Assert.assertEquals(1L, metricRegistry.counter(metricNameFactory.counterName(topic, MetricSubType.CONSUMER_ERROR)).getCount());
+//        Assert.assertEquals(1L, metricRegistry.meter(metricNameFactory.meterName(topic, MetricSubType.CONSUMER)).getCount());
+//        Assert.assertEquals(1L, metricRegistry.meter(metricNameFactory.meterName(topic, MetricSubType.CONSUMER_ERROR)).getCount());
+//
+//    }
+//
+//    @Test
+//    public void disableCollect() {
+//        Config config = this.createConfig(KafkaMessageListenerMetricInterceptor.ENABLE_KEY, "false");
+//        metricRegistry = new MetricRegistry();
+//        kafkaMetric = new KafkaMetric(metricRegistry);
+//        interceptor = new KafkaMessageListenerMetricInterceptor(kafkaMetric, config);
+//        Map<Object, Object> context = ContextUtils.createContext();
+//        AgentInterceptorChain chain = mock(AgentInterceptorChain.class);
+//
+//        interceptor.before(methodInfo, context, chain);
+//        interceptor.after(methodInfo, context, chain);
+//
+//        Assert.assertTrue(metricRegistry.getMetrics().isEmpty());
+//    }
+//}
--- sniffer/src/test/java/com/megaease/easeagent/sniffer/webclient/WebClientBuildAdviceTest.java
+++ sniffer/src/test/java/com/megaease/easeagent/sniffer/webclient/WebClientBuildAdviceTest.java
@@ -1,169 +1,169 @@
-/*
- * Copyright (c) 2017, MegaEase
- * All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.megaease.easeagent.sniffer.webclient;
-
-import com.megaease.easeagent.core.Classes;
-import com.megaease.easeagent.core.Definition;
-import com.megaease.easeagent.core.QualifiedBean;
-import com.megaease.easeagent.core.interceptor.AgentInterceptorChainInvoker;
-import com.megaease.easeagent.sniffer.BaseSnifferTest;
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.client.reactive.ClientHttpConnector;
-import org.springframework.http.codec.ClientCodecConfigurer;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.ExchangeFunction;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriBuilderFactory;
-
-import java.util.List;
-import java.util.Map;
-import java.util.function.Consumer;
-
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.spy;
-
-public class WebClientBuildAdviceTest extends BaseSnifferTest {
-
-    protected static List<Class<?>> classList;
-    protected static AgentInterceptorChainInvoker chainInvoker = spy(AgentInterceptorChainInvoker.getInstance());
-
-    @Before
-    public void before() throws Exception {
-        System.out.println(org.springframework.web.reactive.function.client.WebClient.Builder.class.getName());
-        reset(chainInvoker);
-        if (classList != null) {
-            return;
-        }
-        Definition.Default def = new GenWebClientBuilderAdvice().define(Definition.Default.EMPTY);
-        ClassLoader loader = this.getClass().getClassLoader();
-        classList = Classes.transform(
-                this.getClass().getName() + "$MyBuilder"
-        )
-                .with(def,
-                        new QualifiedBean("supplier4WebClientBuild", this.mockSupplier()),
-                        new QualifiedBean("", chainInvoker))
-                .load(loader);
-
-    }
-
-    @Test
-    public void invoke() throws Exception {
-        MyBuilder builder = (MyBuilder) classList.get(0).newInstance();
-        builder.build();
-        this.verifyInvokeTimes(chainInvoker, 1);
-
-    }
-
-    static class MyBuilder implements WebClient.Builder {
-
-        @Override
-        public WebClient.Builder baseUrl(String baseUrl) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder defaultUriVariables(Map<String, ?> defaultUriVariables) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder uriBuilderFactory(UriBuilderFactory uriBuilderFactory) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder defaultHeader(String header, String... values) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder defaultHeaders(Consumer<HttpHeaders> headersConsumer) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder defaultCookie(String cookie, String... values) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder defaultCookies(Consumer<MultiValueMap<String, String>> cookiesConsumer) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder defaultRequest(Consumer<WebClient.RequestHeadersSpec<?>> defaultRequest) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder filter(ExchangeFilterFunction filter) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder filters(Consumer<List<ExchangeFilterFunction>> filtersConsumer) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder clientConnector(ClientHttpConnector connector) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder codecs(Consumer<ClientCodecConfigurer> configurer) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder exchangeStrategies(ExchangeStrategies strategies) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder exchangeStrategies(Consumer<ExchangeStrategies.Builder> configurer) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder exchangeFunction(ExchangeFunction exchangeFunction) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder apply(Consumer<WebClient.Builder> builderConsumer) {
-            return null;
-        }
-
-        @Override
-        public WebClient.Builder clone() {
-            return null;
-        }
-
-        @Override
-        public WebClient build() {
-            return null;
-        }
-    }
-
-}
+///*
+// * Copyright (c) 2017, MegaEase
+// * All rights reserved.
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//package com.megaease.easeagent.sniffer.webclient;
+//
+//import com.megaease.easeagent.core.Classes;
+//import com.megaease.easeagent.core.Definition;
+//import com.megaease.easeagent.core.QualifiedBean;
+//import com.megaease.easeagent.core.interceptor.AgentInterceptorChainInvoker;
+//import com.megaease.easeagent.sniffer.BaseSnifferTest;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.client.reactive.ClientHttpConnector;
+//import org.springframework.http.codec.ClientCodecConfigurer;
+//import org.springframework.util.MultiValueMap;
+//import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+//import org.springframework.web.reactive.function.client.ExchangeFunction;
+//import org.springframework.web.reactive.function.client.ExchangeStrategies;
+//import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.util.UriBuilderFactory;
+//
+//import java.util.List;
+//import java.util.Map;
+//import java.util.function.Consumer;
+//
+//import static org.mockito.Mockito.reset;
+//import static org.mockito.Mockito.spy;
+//
+//public class WebClientBuildAdviceTest extends BaseSnifferTest {
+//
+//    protected static List<Class<?>> classList;
+//    protected static AgentInterceptorChainInvoker chainInvoker = spy(AgentInterceptorChainInvoker.getInstance());
+//
+//    @Before
+//    public void before() throws Exception {
+//        System.out.println(org.springframework.web.reactive.function.client.WebClient.Builder.class.getName());
+//        reset(chainInvoker);
+//        if (classList != null) {
+//            return;
+//        }
+//        Definition.Default def = new GenWebClientBuilderAdvice().define(Definition.Default.EMPTY);
+//        ClassLoader loader = this.getClass().getClassLoader();
+//        classList = Classes.transform(
+//                this.getClass().getName() + "$MyBuilder"
+//        )
+//                .with(def,
+//                        new QualifiedBean("supplier4WebClientBuild", this.mockSupplier()),
+//                        new QualifiedBean("", chainInvoker))
+//                .load(loader);
+//
+//    }
+//
+//    @Test
+//    public void invoke() throws Exception {
+//        MyBuilder builder = (MyBuilder) classList.get(0).newInstance();
+//        builder.build();
+//        this.verifyInvokeTimes(chainInvoker, 1);
+//
+//    }
+//
+//    static class MyBuilder implements WebClient.Builder {
+//
+//        @Override
+//        public WebClient.Builder baseUrl(String baseUrl) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder defaultUriVariables(Map<String, ?> defaultUriVariables) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder uriBuilderFactory(UriBuilderFactory uriBuilderFactory) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder defaultHeader(String header, String... values) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder defaultHeaders(Consumer<HttpHeaders> headersConsumer) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder defaultCookie(String cookie, String... values) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder defaultCookies(Consumer<MultiValueMap<String, String>> cookiesConsumer) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder defaultRequest(Consumer<WebClient.RequestHeadersSpec<?>> defaultRequest) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder filter(ExchangeFilterFunction filter) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder filters(Consumer<List<ExchangeFilterFunction>> filtersConsumer) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder clientConnector(ClientHttpConnector connector) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder codecs(Consumer<ClientCodecConfigurer> configurer) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder exchangeStrategies(ExchangeStrategies strategies) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder exchangeStrategies(Consumer<ExchangeStrategies.Builder> configurer) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder exchangeFunction(ExchangeFunction exchangeFunction) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder apply(Consumer<WebClient.Builder> builderConsumer) {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient.Builder clone() {
+//            return null;
+//        }
+//
+//        @Override
+//        public WebClient build() {
+//            return null;
+//        }
+//    }
+//
+//}
