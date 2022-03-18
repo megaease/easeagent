59324fd2a854a52234011f50f0806404ac21f8d4
yufu.deng <observeralone@gmail.com>
2022-03-18 16:44:39 +0800

    add mysql prefix to jdbc span.remoteServiceName (#254)
    * add mysql prefix to jdbc span.remoteServiceName
    
    * modify test

--- plugins/jdbc/src/main/java/com/megaease/easeagent/plugin/jdbc/interceptor/tracing/JdbcStmTracingInterceptor.java
+++ plugins/jdbc/src/main/java/com/megaease/easeagent/plugin/jdbc/interceptor/tracing/JdbcStmTracingInterceptor.java
@@ -34,6 +34,7 @@ import com.megaease.easeagent.plugin.interceptor.NonReentrantInterceptor;
 import com.megaease.easeagent.plugin.jdbc.JdbcTracingPlugin;
 import com.megaease.easeagent.plugin.jdbc.advice.JdbcStatementAdvice;
 import com.megaease.easeagent.plugin.jdbc.common.*;
+import com.megaease.easeagent.plugin.utils.common.StringUtils;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import java.sql.Connection;
@@ -83,11 +84,19 @@ public class JdbcStmTracingInterceptor implements NonReentrantInterceptor {
         RedirectProcessor.setTagsIfRedirected(Redirect.DATABASE, span, url);
         DatabaseInfo databaseInfo = DatabaseInfo.getFromConnection(conn);
         if (databaseInfo != null) {
-            span.remoteServiceName(databaseInfo.getDatabase());
+            span.remoteServiceName(remoteServiceName(databaseInfo.getDatabase()));
             span.remoteIpAndPort(databaseInfo.getHost(), databaseInfo.getPort());
         }
         span.start();
         context.put(SPAN_KEY, span);
+    }
+
+    public String remoteServiceName(String database) {
+        if (StringUtils.isEmpty(database)) {
+            return "mysql";
+        } else {
+            return "mysql-" + database;
+        }
     }
 
     @Override
