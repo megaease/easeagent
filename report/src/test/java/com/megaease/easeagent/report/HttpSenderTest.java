909d4dcd2cc6b01eed90ac0c4d511ef7829926a1
landyking <landykingdom@gmail.com>
2022-07-11 15:51:48 +0800

    Update Copyright

--- report/src/test/java/com/megaease/easeagent/report/HttpSenderTest.java
+++ report/src/test/java/com/megaease/easeagent/report/HttpSenderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 MegaEase
+ * Copyright (c) 2022, MegaEase
  * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package com.megaease.easeagent.report;
