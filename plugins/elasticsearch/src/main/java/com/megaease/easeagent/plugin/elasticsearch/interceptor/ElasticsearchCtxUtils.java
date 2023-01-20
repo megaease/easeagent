3ac875bbb3ad76179ddd72cc88c23f5a0caa17f1
yufu.deng <observeralone@gmail.com>
2023-01-20 10:09:25 +0800

    optimization: add component.type to Elasticsearch Span

--- plugins/elasticsearch/src/main/java/com/megaease/easeagent/plugin/elasticsearch/interceptor/ElasticsearchCtxUtils.java
+++ plugins/elasticsearch/src/main/java/com/megaease/easeagent/plugin/elasticsearch/interceptor/ElasticsearchCtxUtils.java
@@ -18,6 +18,8 @@
 package com.megaease.easeagent.plugin.elasticsearch.interceptor;
 
 import com.megaease.easeagent.plugin.api.Context;
+import com.megaease.easeagent.plugin.api.middleware.MiddlewareConstants;
+import com.megaease.easeagent.plugin.api.middleware.Type;
 import com.megaease.easeagent.plugin.api.trace.Span;
 import com.megaease.easeagent.plugin.interceptor.MethodInfo;
 import com.megaease.easeagent.plugin.utils.common.StringUtils;
@@ -40,6 +42,7 @@ public class ElasticsearchCtxUtils {
         Span span = context.nextSpan();
         span.kind(Span.Kind.CLIENT);
         span.remoteServiceName("elasticsearch");
+        span.tag(MiddlewareConstants.TYPE_TAG_NAME, Type.ELASTICSEARCH.getRemoteType());
         span.tag("es.index", getIndex(request.getEndpoint()));
         span.tag("es.operation", request.getMethod() + " " + request.getEndpoint());
         if (entity != null) {
