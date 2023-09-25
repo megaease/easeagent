f2cb5b7d45e20c1849c1b5f7039e5fa9f2b2e1e9
enoch <lanxenet@gmail.com>
2023-09-25 10:57:02 +0800

    feat: add forwarded plugin for independent from Trace

---
+++ plugins/httpurlconnection/src/main/java/com/megaease/easeagent/plugin/httpurlconnection/interceptor/HttpURLConnectionGetResponseCodeForwardedInterceptor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2023, MegaEase
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.megaease.easeagent.plugin.httpurlconnection.interceptor;
+
+import com.megaease.easeagent.plugin.annotation.AdviceTo;
+import com.megaease.easeagent.plugin.api.Context;
+import com.megaease.easeagent.plugin.api.config.ConfigConst;
+import com.megaease.easeagent.plugin.enums.Order;
+import com.megaease.easeagent.plugin.httpurlconnection.ForwardedPlugin;
+import com.megaease.easeagent.plugin.httpurlconnection.HttpURLConnectionPlugin;
+import com.megaease.easeagent.plugin.httpurlconnection.advice.HttpURLConnectionGetResponseCodeAdvice;
+import com.megaease.easeagent.plugin.interceptor.Interceptor;
+import com.megaease.easeagent.plugin.interceptor.MethodInfo;
+
+import java.net.HttpURLConnection;
+
+@AdviceTo(value = HttpURLConnectionGetResponseCodeAdvice.class, qualifier = "default", plugin = ForwardedPlugin.class)
+public class HttpURLConnectionGetResponseCodeForwardedInterceptor implements Interceptor {
+
+    @Override
+    public void before(MethodInfo methodInfo, Context context) {
+        HttpURLConnection httpURLConnection = (HttpURLConnection) methodInfo.getInvoker();
+        context.injectForwardedHeaders(httpURLConnection::setRequestProperty);
+    }
+
+    @Override
+    public String getType() {
+        return ConfigConst.PluginID.FORWARDED;
+    }
+
+    @Override
+    public int order() {
+        return Order.FORWARDED.getOrder();
+    }
+}
