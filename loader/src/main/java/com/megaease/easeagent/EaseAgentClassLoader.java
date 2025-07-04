6feba019bb6f0c004c85b08d6dbde12d34b7ace3
observeralone <observeralone@gmail.com>
2025-07-04 10:23:25 +0800

    reset: add externals to EaseAgentClassLoader and add StringSequence

--- loader/src/main/java/com/megaease/easeagent/EaseAgentClassLoader.java
+++ loader/src/main/java/com/megaease/easeagent/EaseAgentClassLoader.java
@@ -17,14 +17,75 @@
 
 package com.megaease.easeagent;
 
+import java.lang.ref.WeakReference;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * EaseAgent's exclusive classloader, used to isolate classes
  */
 public class EaseAgentClassLoader extends URLClassLoader {
+    static {
+        ClassLoader.registerAsParallelCapable();
+    }
+
+    private final Set<WeakReference<ClassLoader>> externals = new CopyOnWriteArraySet<>();
+
     public EaseAgentClassLoader(URL[] urls, ClassLoader parent) {
         super(urls, parent);
+    }
+
+    @SuppressWarnings("unused")
+    public void add(ClassLoader cl) {
+        if (cl != null && !Objects.equals(cl, this)) {
+            externals.add(new WeakReference<>(cl));
+        }
+    }
+
+    @Override
+    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        try {
+            return super.loadClass(name, resolve);
+        } catch (ClassNotFoundException e) {
+            for (WeakReference<ClassLoader> external : externals) {
+                try {
+                    ClassLoader cl = external.get();
+                    if (cl == null) {
+                        continue;
+                    }
+                    final Class<?> aClass = cl.loadClass(name);
+                    if (resolve) {
+                        resolveClass(aClass);
+                    }
+                    return aClass;
+                } catch (ClassNotFoundException ignored) {
+                    // ignored
+                }
+            }
+
+            throw e;
+        }
+    }
+
+    @Override
+    public URL findResource(String name) {
+        URL url = super.findResource(name);
+        if (url == null) {
+            for (WeakReference<ClassLoader> external : externals) {
+                try {
+                    ClassLoader cl = external.get();
+                    url = cl.getResource(name);
+                    if (url != null) {
+                        return url;
+                    }
+                } catch (Exception ignored) {
+                    // ignored
+                }
+            }
+        }
+        return url;
     }
 }
